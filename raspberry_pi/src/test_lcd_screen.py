1d010fcb558734a12a94000f081a8900b9b9a500
Erick Díaz Saborio <ewdiazsa@gmail.com>
2020-07-28 16:18:01 -0600

    reorganize repository

---
+++ raspberry_pi/src/test_lcd_screen.py
@@ -0,0 +1,20 @@
+import signal
+import sys
+import time
+import queue
+
+from lcd_screen_controller import LCD_16x2_I2C_driver
+from utilities.config import Config
+
+
+i2c_address = int(Config().get(Config.LCD_SCREEN_CONTROLLER_I2C_ADDRESS), 0)
+
+screen = LCD_16x2_I2C_driver.lcd(address=i2c_address)
+
+screen.lcd_clear()
+
+# Test BackLight
+screen.backlight(0)
+time.sleep(0.2)
+screen.backlight(1)
+# Test Text
