1756576fc44e2bcc6baa417629784b1996eebf76
Erick Diaz <ewdiazsa@gmail.com>
2020-08-15 17:44:46 -0600

    servo test with jetson nano

---
+++ jetson_nano/src/spotmicroai/test_servo.py
@@ -0,0 +1,17 @@
+from adafruit_servokit import ServoKit
+import board
+import busio
+import time
+
+# On the Jetson Nano
+# Bus 0 (pins 28,27) is board SCL_1, SDA_1 in the jetson board definition file
+# Bus 1 (pins 5, 3) is board SCL, SDA in the jetson definition file
+# Default is to Bus 1; We are using Bus 0
+print("Initializing Servos")
+i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
+print("Initializing ServoKit")
+kit = ServoKit(channels=16, i2c=i2c_bus0)
+# kit[0] is the bottom servo
+
+kit.servo[0].angle=180
+kit.servo[1].angle=0
